bebff379f2c3e74632723490f4be691a4f75643b
Mark R Baker <mrbaker1917@gmail.com>
2020-12-20 11:36:46 -0800

    adds genres column to Venue

--- projects/01_fyyur/app.py
+++ projects/01_fyyur/app.py
@@ -41,6 +41,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String(500))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
